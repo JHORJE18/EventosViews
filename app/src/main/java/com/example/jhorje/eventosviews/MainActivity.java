c2ab1d5e272a6520458d8919bee90b4ff35b73ec
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-10-25 16:50:51 +0200

    Boton cambiar fondo

--- app/src/main/java/com/example/jhorje/eventosviews/MainActivity.java
+++ app/src/main/java/com/example/jhorje/eventosviews/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.jhorje.eventosviews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     //Variables vista
     private RelativeLayout rl;
+    private Button btnFondo;
 
     //Extra variables
     private boolean fondoTint = false;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Asignamos variables de la vista
         rl = (RelativeLayout) findViewById(R.id.relativaLay);
+        btnFondo = (Button) findViewById(R.id.btnFondo);
     }
 
     public void onClick(View v){
@@ -29,12 +32,14 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()){
             case R.id.btnFondo:
                 //Cambiar fondo de color
-                if (fondoTint){
+                if (!fondoTint){
                     rl.setBackground(getDrawable(R.color.Rojo));
-                    fondoTint = false;
+                    btnFondo.setText("Fondo blanco");
+                    fondoTint = true;
                 } else {
                     rl.setBackground(getDrawable(R.color.Transparente));
-                    fondoTint = true;
+                    btnFondo.setText("Fondo rojo");
+                    fondoTint = false;
                 }
                 break;
         }
